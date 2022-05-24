d1f717531ce807570a7a55577d1369ccc51bb390
Sony Setiawan <sonysetiawanred@gmail.com>
2022-05-24 15:21:23 +0700

    feat: routes companies

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,76 +1,51 @@
-import { collection } from './collection'
+import { collection } from "./collection";
 
-import { countries } from './countries'
+import { countries } from "./countries";
 
-import { users } from './users'
-import { roles } from './roles'
-import { profile } from './profile'
-import { addresses } from './addresses'
-import { contacts } from './contacts'
-import { files } from './files'
-import { sysparam } from './sysparam'
+import { users } from "./users";
+import { roles } from "./roles";
+import { profile } from "./profile";
+import { addresses } from "./addresses";
+import { contacts } from "./contacts";
+import { files } from "./files";
+import { sysparam } from "./sysparam";
 
-import { notifications } from './notifications'
-import { notificationTemplates } from './notificationTemplates'
-import { notificationFormats } from './notificationFormats'
+import { notifications } from "./notifications";
+import { notificationTemplates } from "./notificationTemplates";
+import { notificationFormats } from "./notificationFormats";
+
+import { companies } from "./companies";
 
 const routes = (store) => [
   {
-    path: '',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    path: "",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Index.vue") }],
   },
   {
-    path: '/login',
-    component: () => import('layouts/UserLogin.vue'),
-    children: [
-      { path: '', component: () => import('pages/users/login.vue') }
-    ]
+    path: "/login",
+    component: () => import("layouts/UserLogin.vue"),
+    children: [{ path: "", component: () => import("pages/users/login.vue") }],
   },
   {
-    path: '/register',
-    component: () => import('layouts/UserLogin.vue'),
+    path: "/register",
+    component: () => import("layouts/UserLogin.vue"),
     children: [
-      { path: '', component: () => import('pages/users/register.vue') }
-    ]
+      { path: "", component: () => import("pages/users/register.vue") },
+    ],
   },
   {
-    path: '/forgot-password',
-    component: () => import('layouts/UserLogin.vue'),
-    children: [
-      { path: '', component: () => import('pages/users/forgot.vue') }
-    ]
+    path: "/forgot-password",
+    component: () => import("layouts/UserLogin.vue"),
+    children: [{ path: "", component: () => import("pages/users/forgot.vue") }],
   },
   {
-    path: '/verify-email/:id/:hash',
-    component: () => import('layouts/UserLogin.vue'),
+    path: "/verify-email/:id/:hash",
+    component: () => import("layouts/UserLogin.vue"),
     children: [
-      { path: '', component: () => import('pages/users/verify-email.vue') }
-    ]
+      { path: "", component: () => import("pages/users/verify-email.vue") },
+    ],
   },
-
-  // {
-  //   path: '/profile',
-  //   component: async () => await import('layouts/MainLayout.vue'),
-  //   children: [
-  //     {
-  //       path: '',
-  //       props: {
-  //         collection: 'users'
-  //       },
-  //       component: () => {
-  //         try {
-  //           return import('pages/profile/show.vue')
-  //         } catch (error) {
-  //           return import('pages/resources/show.vue')
-  //         }
-  //       }
-  //     },
-  //   ]
-  // },
-
 
   { ...profile },
 
@@ -87,15 +62,17 @@ const routes = (store) => [
   { ...notificationTemplates },
   { ...notificationFormats },
 
+  { ...companies(store) },
+
   // Default route
   { ...collection(store) },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
 
-export default routes
+export default routes;
